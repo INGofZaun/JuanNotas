5af0e929c773c578b5b4fd05f9cf0594424d40c8
BryanParamo <S19120180@alumnos.itsur.edu.mx>
2024-11-14 18:44:57 -0600

    Gradle chido ahora si

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,46 +1,110 @@
-<?xml version="1.0" encoding="utf-8"?>
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
-xmlns:tools="http://schemas.android.com/tools">
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
+}
 
+android {
+    namespace = "com.ad_coding.noteappcourse"
+    compileSdk = 34
 
-<uses-feature
-android:name="android.hardware.microphone"
-android:required="false" />
+    defaultConfig {
+        applicationId = "com.ad_coding.noteappcourse"
+        minSdk = 29
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
 
-<uses-permission android:name="android.permission.POST_NOTIFICATIONS" />
-<uses-permission android:name="android.permission.RECORD_AUDIO"/>
-<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
-<uses-permission android:name="android.permission.SET_ALARM"/>
-<uses-permission android:name="com.android.alarm.permission.SET_ALARM"/>
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.4"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
 
+dependencies {
 
-<application
-android:name=".NoteApplication"
-android:allowBackup="true"
-android:dataExtractionRules="@xml/data_extraction_rules"
-android:fullBackupContent="@xml/backup_rules"
-android:icon="@mipmap/ic_launcher"
-android:label="@string/app_name"
-android:roundIcon="@mipmap/ic_launcher_round"
-android:supportsRtl="true"
-android:theme="@style/Theme.NoteAppCourse"
-tools:targetApi="31">
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
 
-<receiver android:name=".Alarma.Alarma"
-android:enabled="true"/>
+    // Icons
+    implementation("androidx.compose.material:material-icons-extended:1.3.1")
 
-<activity
-android:name=".MainActivity"
-android:exported="true"
-android:label="@string/app_name"
-android:theme="@style/Theme.NoteAppCourse">
-<intent-filter>
-<action android:name="android.intent.action.MAIN" />
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    implementation("androidx.leanback:leanback:1.0.0")
+    implementation("com.google.android.material:material:1.10.0")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("androidx.hilt:hilt-work:1.0.0")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
-<category android:name="android.intent.category.LAUNCHER" />
-</intent-filter>
-</activity>
-</application>
+    // MATERIAL 3
+    val m3version = "1.2.0-alpha11"
 
-</manifest>
+    implementation("androidx.compose.material3:material3:$m3version")
+    implementation("androidx.compose.material3:material3-window-size-class:$m3version")
+
+    // Runtime
+    implementation("androidx.work:work-runtime-ktx:2.8.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+
+    // Lifecycle
+    val lifecycleVersion = "2.6.0"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+
+    // Room
+    val roomVersion = "2.5.0"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
