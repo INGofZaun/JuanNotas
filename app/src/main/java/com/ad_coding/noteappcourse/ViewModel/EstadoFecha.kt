3c8175950685705807826f16dad8a682431fd9e1
BryanParamo <S19120180@alumnos.itsur.edu.mx>
2024-11-20 20:03:02 -0600

    EstadoFecha

---
+++ app/src/main/java/com/ad_coding/noteappcourse/ViewModel/EstadoFecha.kt
@@ -0,0 +1,12 @@
+package com.ad_coding.noteappcourse.ViewModel
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class EstadoFecha:ViewModel() {
+
+    var estadoFecha by mutableStateOf("")
+}
